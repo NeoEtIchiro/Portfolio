768618476b610e463edc08fda2fd40a0cc0fd7ee
NeoEtIchiro <neoetichiro@gmail.com>
2024-12-13 11:20:20 +0100

    Des ombres s"affiche sur le côté des sections

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,3 +1,23 @@
-angular.module('myApp').controller('MainController', function($scope) {
-    $scope.message = 'Bienvenue sur mon application AngularJS !';
-  });
+angular.module('myApp', [])
+.controller('MainController', ['$scope', function($scope) {
+    $scope.profileImagePath = "../public/profilePic.png"; // Remplacez par le chemin de votre image
+    $scope.name = 'Néo Bougio'; // Remplacez par votre nom et prénom
+
+    $scope.sections = [
+        {
+            title: "Jeux vidéos",
+            color: "#cc0000",
+            items: [{img: "../public/profilePic.png", title: "Jeu 1", description: "Description du jeu 1 fzeuj k fehuiodezjiodze djizeo dez"},
+                    {img: "../public/profilePic.png", title: "Jeu 2", description: "Description du jeu 2"},
+            ]
+        },
+        {
+            title: "Prototypes",
+            color: "#4400aa",
+            items: [{img: "../public/profilePic.png", title: "P 1", description: "Description du jeu 1"},
+                    {img: "../public/profilePic.png", title: "P 2", description: "Description du jeu 2"},
+                    {img: "../public/profilePic.png", title: "P 3", description: "Description du jeu 3"},
+            ]
+        },
+    ]
+}]);
